9bebe1050d29d7cf4c55c647e44a38fbfe2a5d42
Ruslan Kovalsky <ruslan.kovalsky@gmail.com>
2022-09-21 18:22:58 +0300

    before testing v 1.0.9.3

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.URLField()
+    slug = models.SlugField()
     description = models.TextField()
 
     def __str__(self) -> str:
