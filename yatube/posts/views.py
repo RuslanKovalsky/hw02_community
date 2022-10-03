14624b93699290c6aa2029ccfbbbb6d8837624da
Ruslan Kovalsky <ruslan.kovalsky@gmail.com>
2022-10-03 17:58:46 +0300

    before testing v 1.0.9.8 last

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -17,10 +17,11 @@ def index(request):
 
 
 def group_posts(request, slug):
+    template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:COUNT]
     context = {
         'group': group,
         'posts': posts,
     }
-    return render(request, 'template', context=context)
+    return render(request, template, context=context)
