b3a0a4059c65ff04b81981116b76b6d1f365631f
Ruslan Kovalsky <ruslan.kovalsky@gmail.com>
2022-10-06 16:41:32 +0300

    before testing v 1.0.9.8 last

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,27 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
-
-
-COUNT = 10
+from posts.models import Post, Group
 
 
 def index(request):
-    template = 'posts/index.html'
-    title = 'Последние обновления на сайте'
-    posts = Post.objects.order_by('-pub_date')[:COUNT]
+    posts = Post.objects.all()
     context = {
         'posts': posts,
-        'title': title,
     }
-    return render(request, template, context=context)
+    return render(request, 'posts/index.html', context=context)
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:COUNT]
+    posts = Post.objects.filter(group=group).all()
     context = {
         'group': group,
         'posts': posts,
     }
-    return render(request, template, context=context)
+    return render(request, 'posts/group_list.html', context=context)
